92e447179c7e97ca74f0ab3fab4de323b2545723
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2017-06-25 23:17:35 +0800

    Added posts to profile

--- profile_script.js
+++ profile_script.js
@@ -3,4 +3,35 @@ $(document).ready(function(){
 		var url = new URL(string);
 		var userID = url.searchParams.get("userId");
 		alert(userID);
+    
+        //load posts
+        $.get("https://jsonplaceholder.typicode.com/users",function(users){
+        $.get("https://jsonplaceholder.typicode.com/posts",function(obj){
+            var i = 0;
+            var selector = userID * 10;
+                selector += 9 ;
+            while(i < 10){
+                var newDiv = $("<div>"); 
+				var space = $("<br>");
+				  
+				var username = document.createElement("a");
+                username.append(users[userID].name)
+                $(username).attr('href', 'profile.html?userId='+(userID));
+                
+				newDiv.addClass()
+                newDiv.html("<br>"+obj[(selector-i)].title+"<br>"+obj[(selector-i)].body);
+                $("#postsDiv").append(username);
+                $("#postsDiv").append("<br>"); // di ko nilagay space cuz it doesnt work for some reason
+                
+                $("#postsDiv").append(newDiv);
+                $("#postsDiv").append(space);
+                
+                i++;
+            }
+        });
+        
+        });
+        //load album
+    
+        //load photos
 		})
