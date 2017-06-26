7d5f0888887a891daa58def1beca83d4633d8db5
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2017-06-26 11:07:03 +0800

    Added thumbnail image and added thumbnail to profile which would lead u to album.html

--- profile_script.js
+++ profile_script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 		var string = window.location.href;
 		var url = new URL(string);
 		var userID = url.searchParams.get("userId");
+        var defImage = 'album_thumbnail.png';
 		alert(userID);
     
         //load posts
@@ -18,7 +19,6 @@ $(document).ready(function(){
                 username.append(users[userID].name)
                 $(username).attr('href', 'profile.html?userId='+(userID));
                 
-				newDiv.addClass()
                 newDiv.html("<br>"+obj[(selector-i)].title+"<br>"+obj[(selector-i)].body);
                 $("#postsDiv").append(username);
                 $("#postsDiv").append("<br>"); // di ko nilagay space cuz it doesnt work for some reason
@@ -32,6 +32,26 @@ $(document).ready(function(){
         
         });
         //load album
-    
+        $.get("https://jsonplaceholder.typicode.com/albums", function(albums){
+            var i = 0;
+            var selector = userID * 10;
+                selector += 9;
+            
+            while(i < 10){
+                var titleSrc = albums[selector-i].title;
+                
+                $('<img />').attr({
+                    src: defImage,
+                    width: 60,
+                    height: 60,
+                    title: titleSrc
+                }).appendTo($('<a/>').attr({
+                    href: 'album.html?albumID=' + ((albums[selector-i].id)-1)
+                }).appendTo($('#thumbnails')));
+                
+                i++;
+            }
+                
+        });
         //load photos
 		})
